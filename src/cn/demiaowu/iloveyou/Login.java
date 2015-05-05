60f8c4bb927a0ace13f1605b76b1de6796c5e5ec
demiaowu <cfreestar@163.com>
2015-05-05 21:45:40 +0800

    add login

---
+++ src/cn/demiaowu/iloveyou/Login.java
@@ -0,0 +1,50 @@
+package cn.demiaowu.iloveyou;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class Login extends HttpServlet {
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doPost(request, response);
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		HttpSession session = request.getSession();
+		if ((session.getAttribute("login") != null) && "yes".equals(session.getAttribute("login"))) {
+			response.sendRedirect(request.getContextPath()+"/index.jsp");
+		}else {
+			request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+		}
+	}
+
+}
